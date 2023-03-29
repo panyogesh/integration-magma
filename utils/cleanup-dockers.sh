f3a2022f32ef18e7beeae092d92bc05173584778
panyogesh <69527565+panyogesh@users.noreply.github.com>
2023-03-29 09:31:28 +0530

    Update cleanup-dockers.sh

--- utils/cleanup-dockers.sh
+++ utils/cleanup-dockers.sh
@@ -1,7 +1,7 @@
 sudo docker container stop $(sudo docker container ls -aq)
 sudo docker container rm $(sudo docker container ls -aq)
-sudo docker container prune
-sudo docker image prune -a
-sudo docker volume prune
+sudo docker container prune -f
+sudo docker image prune -a -f
+sudo docker volume prune -f
 sudo docker system prune -a
-sudo docker system prune
+sudo docker system prune -f
