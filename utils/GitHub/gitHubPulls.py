55e5a7439206e75176807b235e215db3901ea42d
panyogesh <69527565+panyogesh@users.noreply.github.com>
2023-08-28 18:14:58 +0530

    Create gitHubPulls.py

---
+++ utils/GitHub/gitHubPulls.py
@@ -0,0 +1,8 @@
+# Script for fetching pull requests for open PRs 
+from github import Github
+g = Github('Your Token')
+repo = g.get_repo("magma/magma")
+pulls = repo.get_pulls(state='open', sort='created', base='master')
+for pr in pulls:
+   author=repo.get_pull(pr.number)
+   print('{}, {}, {}'.format(pr.number, pr.title, author.user.login))
