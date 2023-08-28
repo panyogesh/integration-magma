02e0a60b9b7781015ac82edfe743c034a94d6297
panyogesh <69527565+panyogesh@users.noreply.github.com>
2023-08-28 22:07:49 +0530

    Create gitHubFetchClosedPulls.py

---
+++ utils/GitHub/gitHubFetchClosedPulls.py
@@ -0,0 +1,11 @@
+from github import Github
+import datetime
+
+g = Github('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')
+repo = g.get_repo("magma/magma")
+cutdate=datetime.datetime(2023, 1, 1)
+pulls = repo.get_pulls(state='closed', sort='created', direction='desc', base='master')
+for pr in pulls:
+   author=repo.get_pull(pr.number)
+   if pr.merged == False and pr.closed_at > cutdate and 'dependabot' in author.user.login:
+       print('{}, {}, {}, {}'.format(pr.number, pr.title, author.user.login, pr.closed_at))
